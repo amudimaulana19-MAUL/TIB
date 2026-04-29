9349f63b3034633f3729bca87828f3d4f87da0d1
amudimaulana19-MAUL <amudimaulana19@gmail.com>
2026-04-28 18:28:57 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+echo "hello word";
+
+$nama = "udet";
+$umur = 40;
+$tinggi = 170.9;
+$kelas = "TIB Smester 4";
+
+echo "nama saya $nama, umur saya $umur, tinggi saya $tinggi, kelas saya $kelas";
+
+echo "<br><br>===================================<br><br>";
+
+// operator dan kondisi (if else)
+
+$nilai1 = 11;
+$nilai2 = 12;
+$nilai3 =5;
+
+$hasil = $nilai1 * $nilai2 - $nilai3;
+
+echo "hasil dari $nilai1 * $nilai2 - $nilai3 adalah $hasil";
+
+if (hasil >= 100)
+    (echo "<br> hasilnya lebih besar dari 1000");
+ else if (hasil <= 100)
+    echo "<br> hasilnya lebih kecil dari 1000";
+ else
+    echo"<br>nilai anda kosong"
+
+ 
+?>
