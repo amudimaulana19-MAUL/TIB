ad73442fbbb3fb7d0e7bef53b03cb5d41387bbb1
amudimaulana19-MAUL <amudimaulana19@gmail.com>
2026-04-28 19:33:52 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo '<br><br><a href="materi1.php"><button> Ke materi 1 : if dan else</button></a>';
+echo '<br><br><a href="materi2.php"><button> Ke materi 2 : for $i/while & do while </button></a>';
+echo '<br><br><a href="materi3.php"><button> Ke materi 3 : function </button></a>';
+echo '<br><br><a href="materi4.php"><button> Ke materi 4 : function menu login </button></a>';
+echo '<br><br><a href="materi5.php"><button> Ke materi 5 : looping </button></a>';
+echo '<br><br><a href="materi6.php"><button> Ke materi 6 : looping </button></a>';
+echo '<br><br><a href="materi7.php"><button> Ke materi 7 : looping </button></a>';
+echo '<br><br><a href="materi8.php"><button> Ke materi 8 : looping </button></a>';
+echo '<br><br><a href="materi9.php"><button> Ke materi 9 : looping </button></a>';
+echo '<br><br><a href="materi10.php"><button> Ke materi 10 : looping </button></a>';
+?>
